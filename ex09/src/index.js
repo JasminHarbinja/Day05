a84f67ac99cae10a7a687c0e455dfffefab65e0e
Jasmin Harbinja <57997257+JasminHarbinja@users.noreply.github.com>
2021-03-13 00:51:25 +0100

    Add files via upload

---
+++ ex09/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line 
+    var lastName = "Doe";
+    var firstLetterofLastName = " ";
+    firstLetterofLastName = lastName[0];
+    // Only change code above this line
+    return firstLetterofLastName;
+
+}
+
+console.log(main());
+module.exports = main;
